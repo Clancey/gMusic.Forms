07981a5bc5ee4b6aae11d768a5d02a4ee696b12d
James Clancey <james.clancey@gmail.com>
2019-04-17 17:23:10 -0800

    Finally added Gradient Renderer for Android

---
+++ gMusic.Android/Renderers/GradientListViewRenderer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.Forms;
+using gMusic.Views;
+using gMusic.Droid.Renderers;
+using Xamarin.Forms.Platform.Android;
+using Android.Content;
+
+[assembly: ExportRenderer (typeof (GradientListView), typeof (GradientListViewRenderer))]
+namespace gMusic.Droid.Renderers {
+	public class GradientListViewRenderer : ListViewRenderer {
+		public GradientListViewRenderer (Context context ) : base (context)
+		{
+
+		}
+		protected override void OnElementChanged (ElementChangedEventArgs<ListView> e)
+		{
+			base.OnElementChanged (e);
+			if (this.Control != null) {
+				//Control.SetBackgroundColor (Color.Blue.ToAndroid());
+				Control.SetBackgroundResource(gMusic.Droid.Resource.Drawable.gradient);
+			}
+		}
+
+	}
+}
