4d5903954adcede0f4d73a077aecb35cdea7d578
James Clancey <james.clancey@gmail.com>
2019-07-03 15:51:28 -0800

    We now have fancy fonts on the Panarama Control

--- gMusic/Views/Controls/PanaramaView.cs
+++ gMusic/Views/Controls/PanaramaView.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using Xamarin.Forms;
 
@@ -10,37 +11,51 @@ namespace gMusic.Views {
 		ScrollView scrollView;
 		AbsoluteLayout content;
 
+		Color titleColor;
+		Font titleFont;
+		Color selectedColor;
 		public PanaramaView ()
 		{
+			selectedColor = Styles.Styles.CurrentStyle.AccentColor;
+			titleColor = selectedColor.MultiplyAlpha (.25);
+			titleFont = Styles.Styles.CurrentStyle.HeaderTextThinFont;
+			
 			Children.Add (scrollView = new ScrollView {
 				Orientation = ScrollOrientation.Horizontal,
 				Content = content = new AbsoluteLayout (),
-			}) ;
+			});
 
 			scrollView.Scrolled += ScrollView_Scrolled;
+
 			AbsoluteLayout.SetLayoutFlags (scrollView, AbsoluteLayoutFlags.SizeProportional);
 			AbsoluteLayout.SetLayoutBounds (scrollView, new Rectangle (0, 0, 1, 1));
 			AddPage ("Local", new BoxView { BackgroundColor = Color.Green });
 			AddPage ("Google", new BoxView { BackgroundColor = Color.Fuchsia });
+			AddPage ("YouTube", new BoxView { BackgroundColor = Color.Blue });
+			SetTopScroll (true);
 		}
 
 		void ScrollView_Scrolled (object sender, ScrolledEventArgs e)
 		{
-			SetTopScroll ();
+			SetTopScroll (false);
 		}
 		static double headerPadding = 20f;
 
-		void SetTopScroll ()
+		Label lastCurrentButton;
+		void SetTopScroll (bool forceCheck)
 		{
 			if (labels.Count == 0)
 				return;
 			var scrollWidth = this.Width;
 			var scrollOffset = scrollView.ScrollX;
 			var index = scrollWidth == 0 ? 0 : (scrollOffset / scrollWidth);
-			
+
 			var intindex = (int)Math.Max (0, Math.Min (index, labels.Count - 1));
 			var currentButton = labels [intindex];
+			var didChanged = forceCheck || currentButton != lastCurrentButton;
 
+			Debug.WriteLine ($"Current tab did change: {didChanged}");
+			lastCurrentButton = currentButton;
 			//Figure out the exact middle of the screen inside the scrollview
 			var half = scrollWidth / 2;
 			var x = (scrollWidth * index) + half - (currentButton.Width / 2);
@@ -61,12 +76,13 @@ namespace gMusic.Views {
 			var nextIndexes = Enumerable.Range (intindex + 1, labels.Count - intindex - 1);
 
 			//Set the current page frame
-			//currentButton.Font = Parent.SelectedTitleFont;
-			//currentButton.TitleColor = Parent.SelectedTitleColor;
+			if (didChanged) {
+				currentButton.TextColor = selectedColor;
+			}
 			var frame = currentButton.Bounds;
 			frame.X = leftX;
 			frame.Y = halfTop - frame.Height / 2;
-			currentButton.Layout(frame);
+			currentButton.Layout (frame);
 
 
 			var rightX = currentButton.Bounds.Right + headerPadding;
@@ -77,10 +93,10 @@ namespace gMusic.Views {
 				leftX -= frame.Width + headerPadding;
 				frame.X = leftX;
 				frame.Y = halfTop - frame.Height / 2;
-				button.Layout(frame);
-
-				//button.Font = Parent.TitleFont;
-				//button.TitleColor = Parent.TitleColor;
+				button.Layout (frame);
+				if (didChanged) {
+					button.TextColor = titleColor;
+				}
 			}
 
 			//Set the frame of the next labels
@@ -93,9 +109,10 @@ namespace gMusic.Views {
 				frame = button.Bounds;
 				frame.X = rightX;
 				frame.Y = halfTop - frame.Height / 2;
-				button.Layout(frame);
-				//button.Font = Parent.TitleFont;
-				//button.TitleColor = Parent.TitleColor;
+				button.Layout (frame);
+				if (didChanged) {
+					button.TextColor = titleColor;
+				}
 				rightX = frame.Right + headerPadding;
 			}
 		}
@@ -110,17 +127,21 @@ namespace gMusic.Views {
 				SetContentSizes ();
 			}
 		}
-
 		public void AddPage (string title, View view)
 		{
-			var label = new Label { Text = title }.AddTapGesture (ScrollToLabel);
+			var label = new Label {
+				Text = title,
+				Font = titleFont,
+				TextColor = selectedColor,
+			}.AddTapGesture (ScrollToLabel);
+
 			labels.Add (label);
 			content.Children.Add (label);
 
 			views.Add (view);
 			content.Children.Add (view);
 		}
-
+		
 		void ScrollToLabel (Label label)
 		{
 			ScrollToIndex (labels.IndexOf (label));
@@ -155,9 +176,9 @@ namespace gMusic.Views {
 				var v = views [i];
 				bound.X = i * scrollView.Width;
 				v.Layout (bound);
-				
+
 			}
-			SetTopScroll ();
+			SetTopScroll (false);
 		}
 
 	}
