6d6e992c259ae721586ec0f61ac23b254f2d83e2
Sweeky Satpathy <swsat@microsoft.com>
2019-07-10 22:29:17 -0500

    initial UITest commit

--- gMusic.iOS/AppDelegate.cs
+++ gMusic.iOS/AppDelegate.cs
@@ -10,6 +10,7 @@ using UIKit;
 using Xamarin.Forms;
 using gMusic.Managers;
 using Xamarin.Forms.Platform.iOS;
+using System.IO;
 
 namespace gMusic.iOS
 {
@@ -30,62 +31,87 @@ namespace gMusic.iOS
         {
             FFImageLoading.Forms.Platform.CachedImageRenderer.Init();
             Images.AlbumArtScreenSize = (int)NMath.Max(UIScreen.MainScreen.Bounds.Width, UIScreen.MainScreen.Bounds.Height);
+#if DEBUG
+            Xamarin.Calabash.Start();
+#endif
             global::Xamarin.Forms.Forms.Init();
-			NativeInit ();
-			LoadApplication (new App());
-			var result = base.FinishedLaunching(app, options);
-			var inset = app.KeyWindow.SafeAreaInsets;
-			var thick = new Thickness (inset.Left, inset.Top, inset.Right, inset.Bottom);
-			NotificationManager.Shared.ProcInsetAreaChanged (thick);
-			app.KeyWindow.TintColor = Styles.Styles.CurrentStyle.AccentColor.ToUIColor ();
-			return result;
+            NativeInit();
+            LoadApplication(new App());
+            var result = base.FinishedLaunching(app, options);
+            var inset = app.KeyWindow.SafeAreaInsets;
+            var thick = new Thickness(inset.Left, inset.Top, inset.Right, inset.Bottom);
+            NotificationManager.Shared.ProcInsetAreaChanged(thick);
+            app.KeyWindow.TintColor = Styles.Styles.CurrentStyle.AccentColor.ToUIColor();
+            return result;
         }
 
-		void NativeInit()
-		{
-			BassIos.Init ();
-			Utility.DeviceName = Helpers.Device.Name;
-			Utility.IsSimulator = Helpers.Device.IsSim;
-			FadePlayer.CreatePlayer = (pd) => pd.IsVideo ? (Player)new AVMediaPlayer  () : new BassPlayer();
-			ApplyStyles ();
-			NSNotificationCenter.DefaultCenter.AddObserver ((NSString)"UIContentSizeCategoryDidChangeNotification", (n) => {
-				ApplyStyles ();
-			});
+        void NativeInit()
+        {
+            BassIos.Init();
+            Utility.DeviceName = Helpers.Device.Name;
+            Utility.IsSimulator = Helpers.Device.IsSim;
+            FadePlayer.CreatePlayer = (pd) => pd.IsVideo ? (Player)new AVMediaPlayer() : new BassPlayer();
+            ApplyStyles();
+            NSNotificationCenter.DefaultCenter.AddObserver((NSString)"UIContentSizeCategoryDidChangeNotification", (n) =>
+            {
+                ApplyStyles();
+            });
 
-			App.InputTextFunc = (x) => {
-				var alert = new TextInputAlert (x.title);
-				return alert.GetText (GetTopMostViewController ());
-			};
-		}
-		void ApplyStyles()
-		{
-			Styles.Styles.HeaderFontSize = (int)UIFont.PreferredTitle1.PointSize;
-			Styles.Styles.MainTextFontSize = (int)UIFont.PreferredSubheadline.PointSize;
-			Styles.Styles.ButtonFontsize = (int)UIFont.PreferredSubheadline.PointSize;
-			Styles.Styles.DetailFontSize = (int)UIFont.PreferredCaption1.PointSize;
-			Styles.Fonts.NormalFontName = "Heebo-Regular";
-			Styles.Fonts.ThinFontName = "Heebo-Thin";
+            App.InputTextFunc = (x) =>
+            {
+                var alert = new TextInputAlert(x.title);
+                return alert.GetText(GetTopMostViewController());
+            };
+        }
+        void ApplyStyles()
+        {
+            Styles.Styles.HeaderFontSize = (int)UIFont.PreferredTitle1.PointSize;
+            Styles.Styles.MainTextFontSize = (int)UIFont.PreferredSubheadline.PointSize;
+            Styles.Styles.ButtonFontsize = (int)UIFont.PreferredSubheadline.PointSize;
+            Styles.Styles.DetailFontSize = (int)UIFont.PreferredCaption1.PointSize;
+            Styles.Fonts.NormalFontName = "Heebo-Regular";
+            Styles.Fonts.ThinFontName = "Heebo-Thin";
 
-			
-			Styles.Styles.ResetAllFonts ();
-		}
-		public override void OnActivated (UIApplication uiApplication)
-		{
-			base.OnActivated (uiApplication);
-		}
 
-		static UIViewController GetTopMostViewController()
-		{
-			var window = UIApplication.SharedApplication.KeyWindow;
-			var root = window.RootViewController;
-			if (root != null) {
-				var current = root;
-				while (current.PresentedViewController != null) {
-					current = current.PresentedViewController;
-				}
-				return current;
-			}
-			return window.RootViewController;
-		}
-	}
+            Styles.Styles.ResetAllFonts();
+        }
+        public override void OnActivated(UIApplication uiApplication)
+        {
+            base.OnActivated(uiApplication);
+        }
+
+        static UIViewController GetTopMostViewController()
+        {
+            var window = UIApplication.SharedApplication.KeyWindow;
+            var root = window.RootViewController;
+            if (root != null)
+            {
+                var current = root;
+                while (current.PresentedViewController != null)
+                {
+                    current = current.PresentedViewController;
+                }
+                return current;
+            }
+            return window.RootViewController;
+        }
+
+        [Export("myBackdoorMethod")] // notice the colon at the end of the method name
+        public void MyBackdoor()
+        {
+            var filename = "db.db";
+            string ext = Path.GetExtension("db.db");
+            string filenameNoExt = filename.Substring(0, filename.Length - ext.Length);
+            string path = Path.Combine("Resources", filenameNoExt);
+            var resourcePathname = NSBundle.MainBundle.PathForResource(path, ext.Substring(1, ext.Length - 1));
+            //var fStream = new FileStream(resourcePathname, FileMode.Open, FileAccess.Read);
+
+            string BaseDir = Directory.GetParent(Environment.GetFolderPath(Environment.SpecialFolder.Personal)).ToString();
+            string LibDir = Path.Combine(BaseDir, "Library/");
+            string dbPath = Path.Combine(LibDir, "db.db");
+            File.Delete(dbPath);
+
+            File.Copy(resourcePathname, LibDir);
+        }
+    }
 }
