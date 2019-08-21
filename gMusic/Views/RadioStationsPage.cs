6a0438c3df1a2a51403fcec3b7e7a35cea8b5da8
James Clancey <james.clancey@gmail.com>
2019-08-21 14:17:44 -0800

    Radio stations view looks better now!

--- gMusic/Views/RadioStationsPage.cs
+++ gMusic/Views/RadioStationsPage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using gMusic.Data;
 using gMusic.Managers;
 using gMusic.Models;
 using gMusic.ViewModels;
@@ -10,15 +11,17 @@ namespace gMusic.Views
 {
     public class RadioStationsPage : ContentPage
     {
+        Command refreshCommand;
         public RadioStationsPage()
         {
             this.Title = Strings.RadioStations;
+            refreshCommand = new Command(() =>
+            {
+                Console.WriteLine("Please refresh");
+            });
             var panarama = new PanaramaView();
-            //panarama.AddPage(Strings.RecentStations, new SimpleDatabaseListViewPage
-            //{
-            //    ViewModel = new SimpleDatabaseViewModel<RadioStation>()
-            //}
-            //);
+            panarama.AddPage(Strings.RecentStations, CreateView(false));
+            panarama.AddPage(Strings.MyStations, CreateView(true));
             Content = panarama;
 
             //this.ViewModel = new SimpleDatabaseViewModel<RadioStation> {
@@ -41,6 +44,52 @@ namespace gMusic.Views
                     Id = "IFL",
                 });
             return iflTask;
+        }
+
+        ListView CreateView(bool isIncluded)
+        {
+            var group = Database.Main.GetGroupInfo<RadioStation>().Clone();
+            if (isIncluded)
+                group.AddFilter("IsIncluded = 1");
+            else
+            {
+                group.Limit = 10;
+                group.OrderByDesc = true;
+                group.OrderBy = "RecentDateTime";
+                group.GroupBy = "";
+            }
+            var viewModel = new SimpleDatabaseViewModel<RadioStation>()
+            {
+                Source = {
+                    GroupInfo = group,
+                    IsGrouped = isIncluded,
+                }
+               
+            };
+            var listView = new ListView
+            {
+                BindingContext = viewModel,
+                RefreshCommand = refreshCommand,
+                GroupDisplayBinding = new Binding("Display"),
+                GroupShortNameBinding = new Binding("Display"),
+                IsGroupingEnabled = viewModel.Source.IsGrouped,
+                HasUnevenRows = true,
+                ItemsSource = viewModel.Source,
+                ItemTemplate = new DataTemplate(typeof(MediaItemCell)),
+
+            };
+
+            listView.ItemSelected += ListView_ItemSelected;
+            return listView;
+        }
+
+        private async void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var station = e.SelectedItem as RadioStation;
+            if (station == null)
+                return;
+            (sender as ListView).SelectedItem = null;
+            await PlaybackManager.Shared.Play(station);
         }
     }
 }
